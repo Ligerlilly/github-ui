de25f00022344a32c093823a9625ef86fc9bb58d
Guest User <student@epicodus.com>
2015-10-22 19:17:15 -0700

    Add call to github api

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+
+
+    return $.get(`http://api.github.com/orgs/${params.id}/repos`).then( raw => {
+      raw.forEach(r => {
+        r.oldId = r.id;
+        r.id = r.name;
+        return r;
+      });
+      return raw;
+    });
+  },
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('org', this.modelFor('org'))
+  }
 });
