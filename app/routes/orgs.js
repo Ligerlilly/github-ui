06cec431b71275594da2af6ca178440daac9ec8e
Guest User <student@epicodus.com>
2015-10-22 18:27:21 -0700

    Add service and setupController

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+      id: 'facebook' },
+      { id: 'emberjs' },
+      { id: 'netflix'
+    }];
+  },
+  favorites: Ember.inject.service(),
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('items', this.get('favorites.items'));
+  },
+
+  actions: {
+    favorite(orgName) {
+
+      this.get('favorites.items').addObject({id: orgName});
+
+    }
+  }
 });
